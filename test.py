63103cda1efda9bfac2db3fc3f843e6932a1fb2c
Tessa van der Veen <tessa.vanderveen@flevoland.nl>
2026-02-10 11:51:10 +0100

    homepagina originele stijling toegevoegd

--- test.py
+++
@@ -1,8 +0,0 @@
-import cbsodata
-import pandas as pd
-
-woonlasten = pd.DataFrame(
-                cbsodata.get_data('85949NED',
-                                  filters="Woningkenmerken eq 'T001100' and Huishoudenskenmerken eq 'T001139' and Regiokenmerken eq 'NL01    ' or Regiokenmerken eq 'PV24    '",
-                                  select=['Regiokenmerken', 'Perioden', 'EigenaarHuurder', 'TotaleWoonlasten_2', 'NettoWoonlasten_3', 'Woonquote_5']))
-woonlasten
